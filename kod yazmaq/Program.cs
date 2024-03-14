da41c448c8e43a1cd6f4a95aed14f985e493d09c
Vusal01 <vusalalizade2003@gmail.com>
2024-03-14 21:46:09 +0400

    task loops arrays

---
+++ kod yazmaq/Program.cs
@@ -0,0 +1,115 @@
+﻿//1.sual
+//int num = 396;
+//int max = 0;
+//int mertebe = 0;
+//int j = 1;
+//while (num > 0)
+//{
+//    mertebe = num % 10;
+//    if (mertebe > max)
+//    {
+//        max = mertebe;
+//    }
+//    num = num / 10;
+//}
+//Console.WriteLine(max);
+
+
+
+
+//2.sual
+//int num = 8;
+//int result = 0;
+
+//while (num > 1)
+//{
+//    if (num % 2 == 0)
+//    {
+//        num = num / 2;
+//    }
+//    else
+//    {
+//        result = 1;
+//        break;
+//    }
+//}
+
+//if (result == 0)
+//{
+//    Console.WriteLine("2 nin quvvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("2 nin quvveti deyil");
+//}
+
+
+
+//3.sual
+//int[] nums = { 2, 3, 9, 11, 2, 16, 18, 21 };
+//for (int a = 0; a < nums.Length; a++)
+//{
+//    if (nums[a] % 2 != 0 && nums[a] % 3 == 0)
+//    {
+//        Console.WriteLine(nums[a]);
+//    }
+//}
+
+
+//4.sual
+//int[] array = { 5, 9, 12, 11, };
+//int murekkeb = 0;
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//    int num = array[i];
+//    int bolensayi = 0;
+//    for (int j = 1; j <= num; j++)
+//    {
+//        if (num % j == 0)
+//        {
+//            bolensayi++;
+//        }
+//    }
+//    if (bolensayi > 2)
+//    {
+//        murekkeb++;
+//    }
+//}
+
+//Console.WriteLine("murekkeb eded sayi " + murekkeb);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
